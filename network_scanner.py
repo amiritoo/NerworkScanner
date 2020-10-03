e80def454727a0ee789b642b3897c591ce3c8895
admiral <amamadf@gmail.com>
2020-10-03 22:57:56 +0330

    NetworkScanner

--- network_scanner.py
+++ network_scanner.py
@@ -3,6 +3,15 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request= scapy.ARP(pdst= ip)
+    broadcast= scapy.Ether(dst= "ff:ff:ff:ff:ff:ff")
+    arp_req_broadcast= broadcast/arp_request
+
+    #print(arp_req_broadcast.summary())
+        #this line can show u arp message
+
+    #arp_req_broadcast.show()
+        #show us more details of the content of packet
+
 
 scan("192.168.1.1/24")
