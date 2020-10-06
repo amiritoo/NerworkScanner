3c3b5012a506773e5a684d9f0af89bee456a0399
admiral <amamadf@gmail.com>
2020-10-06 22:36:05 +0330

    Network_Scanner

--- network_scanner.py
+++ network_scanner.py
@@ -1,17 +1,12 @@
 
-
 import scapy.all as scapy
 
 def scan(ip):
     arp_request= scapy.ARP(pdst= ip)
     broadcast= scapy.Ether(dst= "ff:ff:ff:ff:ff:ff")
     arp_req_broadcast= broadcast/arp_request
+    answered,unasnwerd = scapy.srp(arp_req_broadcast,timeout = 1)
 
-    #print(arp_req_broadcast.summary())
-        #this line can show u arp message
-
-    #arp_req_broadcast.show()
-        #show us more details of the content of packet
 
 
 scan("192.168.1.1/24")
