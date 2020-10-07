fdf05e002b37a20c2c8c1f19b343223e51544eb6
admiral <amamadf@gmail.com>
2020-10-08 00:00:16 +0330

    network_scanner

--- network_scanner.py
+++ network_scanner.py
@@ -5,11 +5,9 @@ def scan(ip):
     arp_request= scapy.ARP(pdst= ip)
     broadcast= scapy.Ether(dst= "ff:ff:ff:ff:ff:ff")
     arp_req_broadcast= broadcast/arp_request
-    answered_list = scapy.srp(arp_req_broadcast,timeout = 1)[0]
-
+    answered_list = scapy.srp(arp_req_broadcast,timeout = 1, verbose=False)[0]
+    print("IP\t\t\tMAC\n-------------------------------------")
     for element in answered_list :
-        print(element[1].psrc)
-        print(element[1].hwdst)
-        print("---------------------------------------------")
+        print(element[1].psrc + "\t\t\t" + element[1].hwdst)
 
 scan("192.168.1.1/24")
