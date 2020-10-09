6e343b5f2e74a315467a5d0d0e8c98f170730e45
admiral <amamadf@gmail.com>
2020-10-09 19:33:44 +0330

    newtwork_scanner

--- network_scanner.py
+++ network_scanner.py
@@ -1,4 +1,3 @@
-
 import scapy.all as scapy
 
 def scan(ip):
@@ -6,8 +5,18 @@ def scan(ip):
     broadcast= scapy.Ether(dst= "ff:ff:ff:ff:ff:ff")
     arp_req_broadcast= broadcast/arp_request
     answered_list = scapy.srp(arp_req_broadcast,timeout = 1, verbose=False)[0]
-    print("IP\t\t\tMAC\n-------------------------------------")
-    for element in answered_list :
-        print(element[1].psrc + "\t\t\t" + element[1].hwdst)
+    client_list=[]
 
-scan("192.168.1.1/24")
+    for element in answered_list :
+        client_dict= {"IP":element[1].psrc,"MAC":element[1].hwdst}
+        client_list.append(client_dict)
+    return client_list
+
+def print_result(list_of_clients):
+    print("IP\t\t\tMAC\n-------------------------------------")
+    for client in list_of_clients:
+        print(client["IP"] + "\t\t" + client["MAC"])
+
+
+final_result= scan("192.168.1.1/24")
+print_result(final_result)
